dd333f58ee41cb001ba4c2702aacf1e163a39a84
nativodigital <nativodigital@192.168.17.3>
2016-09-22 16:50:20 -0500

    mas cosas al controlador de actualizar producto

--- src/main/webapp/resources/js/inventarioAct.js
+++ src/main/webapp/resources/js/inventarioAct.js
@@ -10,4 +10,28 @@
 
 rroperoApp.controller('updateProduct', function($scope){
     console.log("controlador updateProduct");
+    
+    
+    /*
+     * Campos para actualizar la info del producto
+     */
+    
+    $scope.nombre = "Jean Pierre";
+    $scope.desc = "descripcion de mi producto";
+    $scope.cat = "una categoria";
+    $scope.subcat = "una subcategoria";
+    $scope.subcat_art ="subcategoria de articulo";
+    $scope.marca = "marca patito";
+    $scope.talla = "XL";
+    $scope.cliente = "un cliente";
+    $scope.material = "un material";
+    $scope.estado = "estado de la prenda";
+    $scope.fecha_ent = "fecha de entrega";
+    $scope.shopify = "aa";
+    $scope.precio_final = "$1300";
+    
+    
+    $scope.updateProduct = function(){
+        
+    }
 });
